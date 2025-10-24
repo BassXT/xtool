6f914683829083b389a9f25255533baaf88c4ee7
BassXT <91958461+BassXT@users.noreply.github.com>
2025-10-24 14:28:05 +0200

    Refactor XTool config flow to include device type

--- custom_components/xtool/config_flow.py
+++ custom_components/xtool/config_flow.py
@@ -1,36 +1,74 @@
-import logging
+import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
 
-_LOGGER = logging.getLogger(__name__)
+from .const import DOMAIN, CONF_DEVICE_TYPE, CONF_IP, SUPPORTED_DEVICE_TYPES
 
-class XToolConfigFlow(config_entries.ConfigFlow, domain="xtool"):
-    """Handle a config flow for XTool."""
 
+class XToolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    def __init__(self):
-        self._ip_address = None
-
     async def async_step_user(self, user_input=None):
-        """Handle the initial step of the config flow."""
         if user_input is not None:
-            # Validierung der IP-Adresse
-            self._ip_address = user_input[CONF_NAME]
-            self.name = user_input[CONF_NAME]
+            ip = user_input[CONF_IP]
+            devtype = user_input[CONF_DEVICE_TYPE]
+            await self._async_set_unique_id(f"{devtype}_{ip}")
+            self._abort_if_unique_id_configured()
             return self.async_create_entry(
-                title=self.name,
-                data={"ip_address": self._ip_address},
+                title=user_input[CONF_NAME],
+                data={
+                    CONF_NAME: user_input[CONF_NAME],
+                    CONF_IP: ip,
+                    CONF_DEVICE_TYPE: devtype,
+                },
             )
 
-        return self.async_show_form(
-            step_id="user", data_schema=self._get_schema()
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME): str,
+                vol.Required(CONF_IP): str,
+                vol.Required(CONF_DEVICE_TYPE): vol.In(SUPPORTED_DEVICE_TYPES),
+            }
+        )
+        return self.async_show_form(step_id="user", data_schema=schema)
+
+    async def async_step_import(self, import_config):
+        name = import_config.get(CONF_NAME)
+        ip = import_config[CONF_IP]
+        devtype = import_config[CONF_DEVICE_TYPE]
+        await self._async_set_unique_id(f"{devtype}_{ip}")
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(
+            title=name or f"xTool {devtype}",
+            data={
+                CONF_NAME: name or f"xTool {devtype}",
+                CONF_IP: ip,
+                CONF_DEVICE_TYPE: devtype,
+            },
         )
 
-    def _get_schema(self):
-        """Return the schema for the config flow."""
-        from homeassistant.helpers import config_validation as cv
-        return {
-            CONF_NAME: cv.string,
-        }
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return XToolOptionsFlow(config_entry)
+
+
+class XToolOptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, entry):
+        self._entry = entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        data = self._entry.data
+        opts = self._entry.options
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=opts.get(CONF_NAME, data.get(CONF_NAME, "xTool"))): str,
+                vol.Required(CONF_IP, default=opts.get(CONF_IP, data.get(CONF_IP, ""))): str,
+                vol.Required(CONF_DEVICE_TYPE, default=opts.get(CONF_DEVICE_TYPE, data.get(CONF_DEVICE_TYPE, "f1"))): vol.In(SUPPORTED_DEVICE_TYPES),
+            }
+        )
+        return self.async_show_form(step_id="init", data_schema=schema)
