c91a28401aabb88727bbe4a3fd4e7ac4faf147ac
BassXT <91958461+BassXT@users.noreply.github.com>
2025-10-24 14:29:27 +0200

    Implement xTool integration setup functions

--- custom_components/xtool/__init__.py
+++ custom_components/xtool/__init__.py
@@ -1,2 +1,25 @@
-# __init__.py
-"""The xTool integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+PLATFORMS = ["sensor"]
+DOMAIN = "xtool"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    if DOMAIN in config:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": "import"}, data=config[DOMAIN]
+            )
+        )
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(_update_listener))
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+async def _update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_reload(entry.entry_id)
