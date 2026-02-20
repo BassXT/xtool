8df968f3f25311646d812d3b1620658fb571c5ad
BassXT <91958461+BassXT@users.noreply.github.com>
2026-02-20 15:37:40 +0100

    Update supported device types and intervals

--- custom_components/xtool/const.py
+++ custom_components/xtool/const.py
@@ -5,13 +5,22 @@ PLATFORMS: list[str] = ["sensor", "binary_sensor", "camera"]
 CONF_IP_ADDRESS = "ip_address"
 CONF_DEVICE_TYPE = "device_type"
 
-# nur die unterstützten Modelle
 SUPPORTED_DEVICE_TYPES: dict[str, str] = {
-    "p2": "P2",
-    "f1": "F1",
-    "m1": "M1",
-    "apparel": "Apparel Printer",
+    "P2": "p2",
+    "P3": "p3",
+    "F1": "f1",
+    "F2": "f2",
+    "F2 Ultra": "f2u",
+    "F2 Ultra UV": "f2uuv",
+    "M1": "m1",
+    "M1 Ultra": "m1u",
+    "D1": "d1",
+    "Apparel Printer": "apparel"
 }
 
 MANUFACTURER = "xTool"
-DEFAULT_UPDATE_INTERVAL = 10  # Sekunden
+
+# Polling:
+DEFAULT_UPDATE_INTERVAL = 10          # seconds
+DEFAULT_SLOW_UPDATE_INTERVAL = 120    # seconds
+HTTP_TIMEOUT = 5
